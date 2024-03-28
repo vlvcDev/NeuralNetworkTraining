770a0c87cbfae44fcdf0e2d2a1c34936161e4778
VCordovaMSU <vcordo11@msudenver.edu>
2024-03-28 10:50:00 -0600

    semanticSegmentation masking

---
+++ semanticSegmentation.py
@@ -0,0 +1,163 @@
+import os
+import numpy as np
+import cv2
+import glob as glob
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+
+import warnings
+import logging
+import absl
+
+# Filter absl warnings
+warnings.filterwarnings("ignore", module="absl")
+
+# Capture all warnings in the logging system
+logging.captureWarnings(True)
+
+# Set the absl logger level to 'error' to suppress warnings
+absl_logger = logging.getLogger("absl")
+absl_logger.setLevel(logging.ERROR)
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+def download_and_unzip(url, save_path):
+    print(f"Downloading and extracting assests....", end="")
+
+    # Downloading zip file using urllib package.
+    urlretrieve(url, save_path)
+
+    try:
+        # Extracting zip file using the zipfile package.
+        with ZipFile(save_path) as z:
+            # Extract ZIP file contents in the same directory.
+            z.extractall(os.path.split(save_path)[0])
+
+        print("Done")
+
+    except Exception as e:
+        print("\nInvalid file.", e)
+
+URL = r"https://www.dropbox.com/s/wad5js22fbeo1be/camvid_images.zip?dl=1"
+
+asset_zip_path = os.path.join(os.getcwd(), "camvid_images.zip")
+
+# Download if assest ZIP does not exists.
+if not os.path.exists(asset_zip_path):
+    download_and_unzip(URL, asset_zip_path)
+
+# Display sample images
+image_paths = sorted(glob.glob("camvid_images" + os.sep + "*.png"))
+
+for idx in range(len(image_paths)):
+    print(image_paths[idx])
+
+def load_image(path):
+    image = cv2.imread(path)
+
+    # Convert image in BGR format to RGB.
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Add a batch dimension which is required by the model.
+    image = np.expand_dims(image, axis=0) / 255.0
+
+    return image
+
+images = []
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(16, 12))
+
+for idx, axis in enumerate(ax.flat):
+    image = load_image(image_paths[idx])
+    images.append(image)
+    axis.imshow(image[0])
+    axis.axis("off")
+
+# Define a dictionary to map class ID to class name and class color
+class_index = \
+    {
+         0: [(64, 128, 64),  'Animal'],
+         1: [(192, 0, 128),  'Archway'],
+         2: [(0, 128, 192),  'Bicyclist'],
+         3: [(0, 128, 64),   'Bridge'],
+         4: [(128, 0, 0),    'Building'],
+         5: [(64, 0, 128),   'Car'],
+         6: [(64, 0, 192),   'Cart/Luggage/Pram'],
+         7: [(192, 128, 64), 'Child'],
+         8: [(192, 192, 128),'Column Pole'],
+         9: [(64, 64, 128),  'Fence'],
+        10: [(128, 0, 192),  'LaneMkgs Driv'],
+        11: [(192, 0, 64),   'LaneMkgs NonDriv'],
+        12: [(128, 128, 64), 'Misc Text'],
+        13: [(192, 0, 192),  'Motorcycle/Scooter'],
+        14: [(128, 64, 64),  'Other Moving'],
+        15: [(64, 192, 128), 'Parking Block'],
+        16: [(64, 64, 0),    'Pedestrian'],
+        17: [(128, 64, 128), 'Road'],
+        18: [(128, 128, 192),'Road Shoulder'],
+        19: [(0, 0, 192),    'Sidewalk'],
+        20: [(192, 128, 128),'Sign Symbol'],
+        21: [(128, 128, 128),'Sky'],
+        22: [(64, 128, 192), 'SUV/Pickup/Truck'],
+        23: [(0, 0, 64),     'Traffic Cone'],
+        24: [(0, 64, 64),    'Traffic Light'],
+        25: [(192, 64, 128), 'Train'],
+        26: [(128, 128, 0),  'Tree'],
+        27: [(192, 128, 192),'Truck/Bus'],
+        28: [(64, 0, 64),    'Tunnel'],
+        29: [(192, 192, 0),  'Vegetation Misc'],
+        30: [(0, 0, 0),      'Void'],
+        31: [(64, 192, 0),   'Wall']  
+    }
+
+model_url = "https://tfhub.dev/google/HRNet/camvid-hrnetv2-w48/1"
+print("loading model:", model_url)
+
+seg_model = hub.load(model_url)
+print("\nmodel loaded!")
+
+# Make a prediction using the first image in the list of images.
+pred_mask = seg_model.predict(images[0])
+
+# The predicted mask has the following shape: [B, H, W, C].
+print("Shape of predicted mask:", pred_mask.shape)
+
+# Convert tensor to numpy array.
+pred_mask = pred_mask.numpy()
+
+# The 1st label is the background class added by the model, but we can remove it for this dataset.
+pred_mask = pred_mask[:, :, :, 1:]
+
+# We also need to remove the batch dimension.
+pred_mask = np.squeeze(pred_mask)
+
+# Print the shape to confirm: [H, W, C].
+print("Shape of predicted mask after removal of batch dimension and background class")
+
+# Each channel in `pred_mask` contains the probabilities that the pixels
+# in the original image are associated with the class for that channel.
+plt.figure(figsize=(20, 6))
+
+plt.subplot(1, 3, 1)
+plt.title("Input Image", fontsize=14)
+plt.imshow(np.squeeze(images[0]))
+
+plt.subplot(1, 3, 2)
+plt.title("Predictions for Class: Road", fontsize=14)
+plt.imshow(pred_mask[:, :, 0], cmap="gray") # Class 17 corresponds to the 'road' class
+plt.axis("off")
+
+plt.subplot(1, 3, 3)
+plt.title("Predictions for Class: Sky", fontsize=14)
+plt.imshow(pred_mask[:, :, 21], cmap="gray") # Class 21 corresponds to the 'sky' class
+plt.axis("off")
+
+plt.show()
+
+# remember the above for CAT project
